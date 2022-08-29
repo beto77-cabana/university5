732975ed42488e548849f2a38f01610c42d8dbad
luis <beto77.cabana@gmail.com>
2022-08-29 02:09:27 -0300

    creacion de proyecto - Ejemplos de linQ para utilizarlos mas adelante

---
+++ LinqSnippets/Snippets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinqSnippets
+{
+    public class Snippets
+    {
+        static public void BasicLinQ()
+        {
+            string[] cars =
+            {
+                "Audi A5",
+                "VW Golf",
+                "VW California",
+                "Audi A3",
+                "Fiat Punto",
+                "Seat Ibiza",
+                "Seat León"
+            };
+        }
+    }
+}
