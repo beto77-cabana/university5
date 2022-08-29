0f946d22968a694100940ce5385dd5fe0a613870
luis <beto77.cabana@gmail.com>
2022-08-29 03:26:00 -0300

    Ejemplo con Numeros

--- LinqSnippets/Snippets.cs
+++ LinqSnippets/Snippets.cs
@@ -9,7 +9,7 @@ namespace LinqSnippets
     {
         static public void BasicLinQ()
         {
-            string[] cars =     //lista de cadena de texto
+            string[] cars = //lista de cadena de texto
             {
                 "Audi A5",
                 "VW Golf",
@@ -25,12 +25,27 @@ namespace LinqSnippets
             {
                 Console.WriteLine(car);
             }
+
             // 2. SELECT WHERE car is audi (SELECT AUDIs)
             var audiList = from car in cars where car.Contains("Audi") select car;
             foreach (var audi in audiList)
             {
                 Console.WriteLine(audi);
             }
+        }
+
+        //Numbers Examples
+        static public void LinqNumbers()
+        {
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //Each Number multiplied by 3
+            //take all numbers, but 9
+            //Order numbers by ascending value
+            var processedNumberList =
+                numbers
+                    .Select(num => num * 3) //{3,6,9, etc}
+                    .Where(num => num != 9) //all but the 9
+                    .OrderBy(num => num); //at the end, we order ascending
         }
     }
 }
