928ba4ffc346b36ca35934db8fb14ce3db462070
luis <beto77.cabana@gmail.com>
2022-08-30 05:37:33 -0300

    tarea aun por resolver de 4

---
+++ UniversityApiBackend/Models/DataModels/Services.cs
@@ -0,0 +1,23 @@
+﻿namespace UniversityApiBackend.Models.DataModels
+{
+    public class Services
+    {
+        // Buscar usuarios por email
+
+        //Buscar alumnos mayores de edad
+
+           // Buscar alumnos que tengan al menos un curso
+
+            //Buscar cursos de un nivel determinado que al menos tengan un alumno inscrito
+
+       // Buscar cursos de un nivel determinado que sean de una categoría determinada
+
+        //Buscar cursos sin alumnos
+
+        static public void Funciones()
+        {
+            // TO DO EXERCICE
+
+        }
+    }
+}
